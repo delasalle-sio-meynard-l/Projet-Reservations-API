8c95e393bd8211aa755f51537beef7bdd804880f
delasalle-sio-meynard-l <delasalle.sio.meynard.l@gmail.com>
2018-03-27 14:42:24 +0200

    Ajout Reservation.java et sa classe Test

---
+++ src/reservations/tests/ReservationTest.java
@@ -0,0 +1,54 @@
+package reservations.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReservationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeStamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStartTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRoomName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDigicode() {
+		fail("Not yet implemented");
+	}
+
+}
