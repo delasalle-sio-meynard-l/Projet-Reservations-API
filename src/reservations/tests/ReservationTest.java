6fa149198621836aabb4ce5e1a4eb8dde4a0ea39
delasalle-sio-meynard-l <delasalle.sio.meynard.l@gmail.com>
2018-03-27 14:49:01 +0200

    Ajout Utilisateur.java et fin classe Test de Reservation.java

--- src/reservations/tests/ReservationTest.java
+++ src/reservations/tests/ReservationTest.java
@@ -1,54 +1,96 @@
 package reservations.tests;
 
 import static org.junit.Assert.*;
+import java.text.ParseException;
 
-import org.junit.After;
+import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ReservationTest {
+import reservations.classes.Outils;
+import reservations.classes.Reservation;
 
+
+public class ReservationTest {
+	private Reservation uneReservation;
+	
+	private String formatDateUS = "yyyy-MM- dd HH:mm:ss";
+	
 	@Before
 	public void setUp() throws Exception {
+		Date unTimeStamp = Outils.ConvertirEnDate("2014-12- 09 10:30:40", formatDateUS);
+		Date unStartTime = Outils.ConvertirEnDate("2014-12- 09 20:00:00", formatDateUS);
+		Date unEndTime = Outils.ConvertirEnDate("2014-12- 09 22:00:00", formatDateUS);
+		uneReservation = new Reservation(10, unTimeStamp, unStartTime, unEndTime, "Salle info", 4, "123456");
 	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
+	public void testGetId() {assertEquals("Test getId", 10, uneReservation.getId());
+		uneReservation.setId(20);
+		assertEquals("Test setId", 20, uneReservation.getId());
 	}
-
+	
 	@Test
-	public void testGetTimeStamp() {
-		fail("Not yet implemented");
+	public void testGetTimeStamp() throws ParseException {
+		String valeurLue = Outils.FormaterDateHeure(uneReservation.getTimeStamp(), formatDateUS);
+		assertEquals("Test getTimeStamp", "2014-12- 09 10:30:40", valeurLue);
+		Date unNouveauTimeStamp = Outils.ConvertirEnDate("2015-12- 09 11:30:00", formatDateUS);
+		uneReservation.setTimeStamp(unNouveauTimeStamp);
+		valeurLue = Outils.FormaterDateHeure(uneReservation.getTimeStamp(), formatDateUS);
+		assertEquals("Test getTimeStamp", "2015-12- 09 11:30:00", valeurLue);
 	}
-
+	
 	@Test
-	public void testGetStartTime() {
-		fail("Not yet implemented");
+	public void testGetStartTime() throws ParseException {
+		String valeurLue = Outils.FormaterDateHeure(uneReservation.getStartTime(), formatDateUS);
+		assertEquals("Test getStartTime", "2014-12- 09 20:00:00", valeurLue);
+		Date unNouveauStartTime = Outils.ConvertirEnDate("2015-12- 09 11:30:00", formatDateUS);
+		uneReservation.setStartTime(unNouveauStartTime);
+		valeurLue = Outils.FormaterDateHeure(uneReservation.getStartTime(), formatDateUS);
+		assertEquals("Test getStartTime", "2015-12- 09 11:30:00", valeurLue);
 	}
-
+	
 	@Test
-	public void testGetEndTime() {
-		fail("Not yet implemented");
+	public void testGetEndTime() throws ParseException {
+		String valeurLue = Outils.FormaterDateHeure(uneReservation.getEndTime(), formatDateUS);
+		assertEquals("Test getEndTime", "2014-12- 09 22:00:00", valeurLue);
+		Date unNouveauEndTime = Outils.ConvertirEnDate("2015-12- 09 11:30:00", formatDateUS);
+		uneReservation.setEndTime(unNouveauEndTime);
+		valeurLue = Outils.FormaterDateHeure(uneReservation.getEndTime(), formatDateUS);
+		assertEquals("Test getEndTime", "2015-12- 09 11:30:00", valeurLue);
 	}
-
+	
 	@Test
 	public void testGetRoomName() {
-		fail("Not yet implemented");
+		assertEquals("Test getRoomName", "Salle info", uneReservation.getRoomName());
+		uneReservation.setRoomName("Salle informatique ");
+		assertEquals("Test setRoomName", "Salle informatique ", uneReservation.getRoomName());
 	}
-
+	
 	@Test
 	public void testGetStatus() {
-		fail("Not yet implemented");
+		assertEquals("Test getStatus", 4, uneReservation.getStatus());
+		uneReservation.setStatus(8);
+		assertEquals("Test setStatus", 8, uneReservation.getStatus());
 	}
-
+	
 	@Test
 	public void testGetDigicode() {
-		fail("Not yet implemented");
+		assertEquals("Test getDigicode", "123456", uneReservation.getDigicode());
+		uneReservation.setDigicode("abcdef");
+		assertEquals("Test setDigicode", "abcdef", uneReservation.getDigicode());
 	}
-
+	
+	@Test
+	public void testToString() {
+		String msg = "";
+		msg += "id :\t\t" + "10" + "\n";
+		msg += "timestamp :\t" + "2014-12- 09 10:30:40" + "\n";
+		msg += "start_time :\t" + "2014-12- 09 20:00:00" + "\n";
+		msg += "end_time :\t" + "2014-12- 09 22:00:00" + "\n";
+		msg += "room_name :\t" + "Salle info" + "\n";
+		msg += "status :\t\t" + "4" + "\n";
+		msg += "digicode :\t" + "123456" + "\n";
+		assertEquals("Test toString", msg, uneReservation.toString());
+	}
 }
